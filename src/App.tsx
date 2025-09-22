a39ce26a21d997b93776f8601536f2282749afac
서연 <s25037@gsm.hs.kr>
2025-09-22 19:46:48 +0900

    feat: 회원가입, 로그인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
