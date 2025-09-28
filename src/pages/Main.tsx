797e746301338a1969451c248b8ad6ad22a6ebbd
서연 <s25037@gsm.hs.kr>
2025-09-29 08:29:54 +0900

    불필요한 라이브러리 삭제

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { supabase } from '../lib/supabase';
 import { useNavigate } from 'react-router-dom';
